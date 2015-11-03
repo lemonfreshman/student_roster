8dec0f8d622bd436daa6e407910af80f7bdaba13
Riley Jones <rileymjones@gmail.com>
2015-11-02 17:38:51 -0700

    Added student validations [#106908282]

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,2 +1,11 @@
 class Student < ActiveRecord::Base
+  validates :first_name,
+    :last_name, :email, :cell_phone, :home_phone, :work_phone, presence: true
+
+  validates_each :cell_phone, :work_phone, :home_phone do |record, attr, value|
+    record.errors.add attr, 'phone numbers should be 10 numbers long.' if value.length != 10
+  end
+
+  validates :email, format: /@/
+
 end
